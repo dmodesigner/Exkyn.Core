864ba8e133d3d0e6f455e8138591ef3e7acaedc5
Daniel Moura <daniel.dmo@live.com>
2020-08-22 18:28:53 -0300

    Adicionado a classe e os métodos para trabalhar com dinheiro. por extenso.

---
+++ Exkyn.Core/Helpers/MoneyHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace Exkyn.Core.Helpers
+{
+    public static class MoneyHelpers
+    {
+        public static string Coin(decimal value) => string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", value);
+
+        public static string Coin(double value) => string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", value);
+
+        public static string Coin(decimal value, string culture) => string.Format(CultureInfo.GetCultureInfo(culture), "{0:C}", value);
+
+        public static string Coin(double value, string culture) => string.Format(CultureInfo.GetCultureInfo(culture), "{0:C}", value);
+    }
+}
