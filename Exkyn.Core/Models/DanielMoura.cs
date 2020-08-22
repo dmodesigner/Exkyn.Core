c4577a2444a50fbd83319647392b2196969ed57f
Daniel Moura <daniel.dmo@live.com>
2020-08-22 15:00:31 -0300

    Adicionado as models do Designer, Daniel Moiura e Estados do Brasil.

---
+++ Exkyn.Core/Models/DanielMoura.cs
@@ -0,0 +1,15 @@
+﻿namespace Exkyn.Core.Models
+{
+    public class DanielMoura
+    {
+        public const string Nome = "Daniel Moura";
+        public const string Email = "daniel.dmo@live.com";
+        public const string Url = "https://www.dmo.dev.br";
+
+        public const string Facebook = "https://www.facebook.com/daniel.dmo/";
+        public const string Twitter = "https://twitter.com/daniel_dmo/";
+        public const string Linkedin = "https://www.linkedin.com/in/danieldmo/";
+        public const string GitHub = "https://github.com/dmodesigner/";
+        public const string Youtube = "https://www.youtube.com/channel/UCGL9_x8wo3X5aaa5SqGJXaA/";
+    }
+}
---
+++ Exkyn.Core/Models/Designer.cs
@@ -0,0 +1,13 @@
+﻿namespace Exkyn.Core.Models
+{
+    public class Designer
+    {
+        public const string Nome = "DMO Designer";
+        public const string Email = "daniel@dmodesigner.com.br";
+        public const string Url = "https://www.dmodesigner.com.br";
+
+        public const string Facebook = "https://www.facebook.com/DmoDesigner/";
+        public const string Twitter = "https://twitter.com/dmodesigner";
+        public const string Youtube = "https://www.youtube.com/channel/UCrDecyh3MogpV5el5d264jw";
+    }
+}
