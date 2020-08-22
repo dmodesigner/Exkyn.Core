774ddfe6e19f16b9c6b834a70c60d263ea6c9730
Daniel Moura <daniel.dmo@live.com>
2020-08-22 18:20:56 -0300

    Adicionado a classe e métdos para lidar com dias utéis.

---
+++ Exkyn.Core/Helpers/ExpirationDay.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exkyn.Core.Helpers
+{
+    public static class ExpirationDay
+    {
+        public static DateTime Run(int day, DateTime date) => date.AddDays(day);
+
+        public static DateTime Useful(int day, DateTime date)
+        {
+            var count = 1;
+
+            while (count <= day)
+            {
+                var weekday = true;
+
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    weekday = false;
+
+                if (weekday)
+                    count++;
+
+                date = date.AddDays(1);
+            }
+
+            return date;
+        }
+
+        public static DateTime Useful(int day, DateTime date, List<DateTime> holidays)
+        {
+            var count = 1;
+
+            while (count <= day)
+            {
+                var weekday = true;
+
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    weekday = false;
+
+                else if (holidays.Contains(date))
+                    weekday = false;
+
+                if (weekday)
+                    count++;
+
+                date = date.AddDays(1);
+            }
+
+            return date;
+        }
+    }
+}
