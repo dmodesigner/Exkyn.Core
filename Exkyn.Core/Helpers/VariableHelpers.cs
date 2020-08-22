0304a0650ff9fa21e64256b953e447252a02a1d7
Daniel Moura <daniel.dmo@live.com>
2020-08-22 18:43:45 -0300

    Adicionado a classe e seus métodos: - Validar os documentos BR: CPF, CNPJ, PIS e E-mail. - Tratando as variaveis para ficar sem formatações e deixando a primeira letra maiuscula.

---
+++ Exkyn.Core/Helpers/VariableHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exkyn.Core.Helpers
+{
+    public static class VariableHelpers
+    {
+        public static string NoFormatting(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return null;
+
+            input = input.Trim();
+
+            input = Regex.Replace(input, "[^0-9a-zA-Z]+", "");
+
+            return input;
+        }
+
+        public static string UpperFirstLetter(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return null;
+
+            input = input.Trim();
+
+            input = char.ToUpper(input[0]) + input.Substring(1);
+
+            return input;
+        }
+    }
+}
