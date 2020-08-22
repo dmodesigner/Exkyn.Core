53f34a288d9417c9f026b1b7115ba873f6995241
Daniel Moura <daniel.dmo@live.com>
2020-08-22 18:24:34 -0300

    Adicionado a classe com os métodos para tratar as horas inicial e final.

---
+++ Exkyn.Core/Helpers/HourHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exkyn.Core.Helpers
+{
+    public static class HourHelpers
+    {
+        public static DateTime Initial(DateTime date) => Convert.ToDateTime(String.Format("{0:yyyy-MM-dd 00:00:00}", date));
+
+        public static DateTime Final(DateTime date) => Convert.ToDateTime(String.Format("{0:yyyy-MM-dd 23:59:59}", date));
+    }
+}
