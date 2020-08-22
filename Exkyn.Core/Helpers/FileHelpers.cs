1ec39f614c0580c65da6dc10c1f04581d006ce3d
Daniel Moura <daniel.dmo@live.com>
2020-08-22 17:12:05 -0300

    Adicionado as classes com seus métodos: Diretorio. Arquivo. Log.

---
+++ Exkyn.Core/Helpers/FileHelpers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Exkyn.Core.Helpers
+{
+    public static class FileHelpers
+    {
+        #region Métodos Privados
+
+        private static void ValidateFileDirectory(string directory, string file)
+        {
+            if (string.IsNullOrEmpty(directory))
+                throw new ArgumentException("Você deve informar um caminho de diretório.");
+
+            if (string.IsNullOrEmpty(file))
+                throw new ArgumentException("Você deve informar o nome do arquivo.");
+        }
+
+        #endregion
+
+        #region Métodos Públicos
+
+        public static void Create(string directory, string file)
+        {
+            DirectoryHelpers.AddSlashEnd(ref directory);
+
+            ValidateFileDirectory(directory, file);
+
+            if (!File.Exists(directory + file))
+                File.Create(directory + file).Close();
+        }
+
+        public static bool Exist(string directory, string file)
+        {
+            DirectoryHelpers.AddSlashEnd(ref directory);
+
+            ValidateFileDirectory(directory, file);
+
+            return File.Exists(directory + file);
+        }
+
+        public static string ConvertToString(string directory, string file)
+        {
+            DirectoryHelpers.AddSlashEnd(ref directory);
+
+            ValidateFileDirectory(directory, file);
+
+            if (!File.Exists(directory + file))
+                throw new ArgumentException($"O arquivo ({directory}{file}) não foi encontrado.");
+
+            return File.ReadAllText(directory + file);
+        }
+
+        #endregion
+    }
+}
