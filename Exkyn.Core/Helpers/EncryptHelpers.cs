b3df5297b7097cc9c582b59e4dc2b64b04c9fe82
Daniel Moura <daniel.dmo@live.com>
2023-08-26 11:06:22 -0300

    Removido métodos obsoletos de criptografia. Adicionado métodos atualizados de criptofrafia.

--- Exkyn.Core/Helpers/EncryptHelpers.cs
+++ Exkyn.Core/Helpers/EncryptHelpers.cs
@@ -2,24 +2,18 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Exkyn.Core.Helpers
 {
     public static class EncryptHelpers
     {
-        #region Tokens
-
-        private static byte[] bIV = { 0x50, 0x08, 0xF1, 0xDD, 0xDE, 0x3C, 0xF2, 0x18, 0x44, 0x74, 0x19, 0x2C, 0x53, 0x49, 0xAB, 0xBC };
-        private const string tokenCriptografiaBase64 = "Dj3pCz3yk9bsd5f9d54f8h1i5jb2gUm8sRhZwgLyBrV=";
-
-        #endregion
-
         #region Métodos Privados
 
         private static void Validate(string input)
         {
             if (string.IsNullOrEmpty(input))
-                throw new ArgumentException("Não é possível criptografar uma variável vazia ou nula.");
+                throw new ArgumentException("Não é possível criptografar / descriptografar uma variável vazia ou nula.");
         }
 
         #endregion
@@ -43,68 +37,57 @@ namespace Exkyn.Core.Helpers
             return sb.ToString();
         }
 
-        public static string EncryptBase64(string input)
+        public static byte[] AesEncryptor(string input, byte[] key, byte[] iv)
         {
             Validate(input);
 
-            byte[] bKey = Convert.FromBase64String(tokenCriptografiaBase64);
-            byte[] bText = new UTF8Encoding().GetBytes(input);
+            byte[] inputBytes;
 
-            // Instancia a classe de criptografia Rijndael
-            Rijndael rijndael = new RijndaelManaged();
-
-            // Define o tamanho da chave "256 = 8 * 32"                          
-            // 128 (16 caracteres), 192 (24 caracteres) e 256 (32 caracteres)                
-            rijndael.KeySize = 256;
-
-            // Cria o espaço de memória para guardar o valor criptografado:                
-            MemoryStream mStream = new MemoryStream();
-
-            // Instancia o encriptador                 
-            CryptoStream encryptor = new CryptoStream(mStream, rijndael.CreateEncryptor(bKey, bIV), CryptoStreamMode.Write);
-
-            // Faz a escrita dos dados criptografados no espaço de memória
-            encryptor.Write(bText, 0, bText.Length);
-
-            // Despeja toda a memória.                
-            encryptor.FlushFinalBlock();
-
-            // Pega o vetor de bytes da memória e gera a string criptografada                
-            return Convert.ToBase64String(mStream.ToArray());
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Mode = CipherMode.CBC;
+                aes.Key = key;
+                aes.IV = iv;
+                
+                using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                {
+                    var inputBuffer = Encoding.UTF8.GetBytes(input);
+                    inputBytes = encryptor.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
+                }
+            }
+            
+            return inputBytes;
         }
 
-        public static string DecryptBase64(string input)
+        public static string AesDecryptor(byte[] input, byte[] key, byte[] iv)
         {
-            Validate(input);
+            var r = string.Empty;
 
-            // Cria instancias de vetores de bytes com as chaves                
-            byte[] bKey = Convert.FromBase64String(tokenCriptografiaBase64);
-            byte[] bText = Convert.FromBase64String(input);
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Mode = CipherMode.CBC;
 
-            // Instancia a classe de criptografia Rijndael                
-            Rijndael rijndael = new RijndaelManaged();
+                using (ICryptoTransform decryptor = aes.CreateDecryptor(key, iv))
+                {
 
-            // Define o tamanho da chave "256 = 8 * 32"                         
-            // 128 (16 caracteres), 192 (24 caracteres) e 256 (32 caracteres)                
-            rijndael.KeySize = 256;
+                    using (MemoryStream msDecrypt = new MemoryStream(input))
+                    {
 
-            // Cria o espaço de memória para guardar o valor DEScriptografado:               
-            MemoryStream mStream = new MemoryStream();
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
 
-            // Instancia o Decriptador                 
-            CryptoStream decryptor = new CryptoStream(mStream, rijndael.CreateDecryptor(bKey, bIV), CryptoStreamMode.Write);
+                            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                r = srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
 
-            // Faz a escrita dos dados criptografados no espaço de memória   
-            decryptor.Write(bText, 0, bText.Length);
-
-            // Despeja toda a memória.                
-            decryptor.FlushFinalBlock();
-
-            // Instancia a classe de codificação para que a string venha de forma correta         
-            UTF8Encoding utf8 = new UTF8Encoding();
-
-            // Com o vetor de bytes da memória, gera a string descritografada em UTF8       
-            return utf8.GetString(mStream.ToArray());
+            return r;
         }
 
         #endregion
