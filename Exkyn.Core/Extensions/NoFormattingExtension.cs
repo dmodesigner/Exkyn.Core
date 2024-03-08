c5c8e522dd10a695f73692c2e2c090df8732f9fe
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2024-03-08 16:33:49 -0300

    Criado o método para retornar apenas caracteres alfanuméricos

---
+++ Exkyn.Core/Extensions/NoFormattingExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exkyn.Core.Extensions;
+
+public static class NoFormattingExtension
+{
+    /// <summary>
+    /// Retorna a variável sem caracteres especiais
+    /// </summary>
+    /// <param name="value">Recebe a variável ou texto como parâmetro de entrada</param>
+    /// <returns>Se for vazia, nula ou tenha apenas espaço em brancos retornara "null", caso contrário retornara a mesma informação sem formatações e caracteres especiais</returns>
+    public static string NoFormatting(this string value) => string.IsNullOrWhiteSpace(value) 
+        ? null 
+        : Regex.Replace(value.Trim(), "[^0-9a-zA-Z]+", "");
+}
