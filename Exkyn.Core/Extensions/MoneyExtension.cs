fec7ff00cb17153b3ae1724de65366dc7ee294af
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2024-03-08 17:47:52 -0300

    Craido a extensão para trabalhar com valores monetarios formatados.

---
+++ Exkyn.Core/Extensions/MoneyExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Exkyn.Core.Extensions;
+
+public static class MoneyExtension
+{
+    /// <summary>
+    /// Retorna o valor monetário formatado no padrão de moeda brasileiro
+    /// </summary>
+    /// <param name="value">Recebe um decimal</param>
+    /// <returns>Retorna uma string</returns>
+    public static string Money(this decimal value) => string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", value);
+
+    /// <summary>
+    /// Retorna o valor monetário formatado no padrão de moeda brasileiro
+    /// </summary>
+    /// <param name="value">>Recebe um decimal</param>
+    /// <param name="culture">Recebe uma string</param>
+    /// <returns>Retorna uma string</returns>
+    public static string Money(this decimal value, string culture) => string.Format(CultureInfo.GetCultureInfo(culture), "{0:C}", value);
+
+    /// <summary>
+    /// Retorna o valor monetário formatado no padrão de moeda brasileiro
+    /// </summary>
+    /// <param name="value">Recebe um double</param>
+    /// <returns>Retorna uma string</returns>
+    public static string Money(this double value) => string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", value);
+
+    /// <summary>
+    /// Retorna o valor monetário formatado no padrão de moeda brasileiro
+    /// </summary>
+    /// <param name="value">>Recebe um double</param>
+    /// <param name="culture">Recebe uma string</param>
+    /// <returns>Retorna uma string</returns>
+    public static string Money(this double value, string culture) => string.Format(CultureInfo.GetCultureInfo(culture), "{0:C}", value);
+}
