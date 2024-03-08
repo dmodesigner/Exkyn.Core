c30ae77f17c109da90c1d301b12c381b27efd356
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2024-03-08 18:06:59 -0300

    Adicionado summary nos métodos de ExpirationDay

--- Exkyn.Core/Helpers/ExpirationDayHelpers.cs
+++ Exkyn.Core/Helpers/ExpirationDayHelpers.cs
@@ -5,8 +5,20 @@ namespace Exkyn.Core.Helpers
 {
     public static class ExpirationDayHelpers
     {
+        /// <summary>
+        /// Acrescenta uma data corrida a data fornecida
+        /// </summary>
+        /// <param name="day">Recebe um int</param>
+        /// <param name="date">Recebe um DateTime</param>
+        /// <returns>Retorna um DateTime</returns>
         public static DateTime Run(int day, DateTime date) => date.AddDays(day);
 
+        /// <summary>
+        /// Acrescenta dias a data levando em consideração dias uteis
+        /// </summary>
+        /// <param name="day">Recebe um int</param>
+        /// <param name="date">Recebe um DateTime</param>
+        /// <returns>Retorna um DateTime</returns>
         public static DateTime Useful(int day, DateTime date)
         {
             var count = 1;
@@ -27,6 +39,13 @@ namespace Exkyn.Core.Helpers
             return date;
         }
 
+        /// <summary>
+        /// Acrescenta dias a data levando em consideração dias uteis e feriados
+        /// </summary>
+        /// <param name="day">Recebe um int<</param>
+        /// <param name="date">Recebe um DateTime</param>
+        /// <param name="holidays">Recebe uma lista de DateTime</param>
+        /// <returns>Retorna um DateTime</returns>
         public static DateTime Useful(int day, DateTime date, List<DateTime> holidays)
         {
             var count = 1;
