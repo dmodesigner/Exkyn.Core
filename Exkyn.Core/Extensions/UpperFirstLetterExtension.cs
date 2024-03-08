51a400e9e2826de8991899504b8bbdd09b3deae4
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2024-03-08 16:45:01 -0300

    Ajuste para retornar a primeira letra da palavra em maiúscula

--- Exkyn.Core/Extensions/UpperFirstLetterExtension.cs
+++ Exkyn.Core/Extensions/UpperFirstLetterExtension.cs
@@ -1,15 +1,19 @@
-﻿using System.Text.RegularExpressions;
-
-namespace Exkyn.Core.Extensions;
+﻿namespace Exkyn.Core.Extensions;
 
 public static class UpperFirstLetterExtension
 {
     /// <summary>
-    /// Retorna a primeira letra de cada palavra em maiúsculo
+    /// Retorna a primeira letra da palavra em maiúsculo
     /// </summary>
     /// <param name="value">Recebe a variável ou texto como parâmetro de entrada</param>
     /// <returns>Se for vazia, nula ou contenha apenas espaço em brancos retornara "null", caso contrário retornara a mesma informação com as primeiras letras de cada palavra em maiúsculo</returns>
-    public static string UpperFirstLetter(this string value) => string.IsNullOrWhiteSpace(value) 
-        ? null
-        : Regex.Replace(value, "[^0-9a-zA-Z]+", "");
+    public static string UpperFirstLetter(this string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return null;
+
+        value = value.Trim();
+
+        return char.ToUpper(value[0]) + value.Substring(1);
+    }
 }
