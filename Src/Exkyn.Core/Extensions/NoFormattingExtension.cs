51d8d3ba668cd8fb6ca65778c2548a33abd1d49f
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2024-03-12 11:12:35 -0300

    Nova organização do projeto. Adicionado as extensões.

---
+++ Src/Exkyn.Core/Extensions/NoFormattingExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exkyn.Core.Extensions;
+
+public static class NoFormattingExtension
+{
+    /// <summary>
+    /// Retorna a variável sem caracteres especiais
+    /// </summary>
+    /// <param name="value">Recebe uma string</param>
+    /// <returns>Retorna uma string</returns>
+    public static string? NoFormatting(this string value) => string.IsNullOrWhiteSpace(value)
+        ? null
+        : Regex.Replace(value.Trim(), "[^0-9a-zA-Z]+", "");
+}
